c3610ab0544532fb4143abc6674030761b3a55a9
Leonardo Isso <leoisso.work@gmail.com>
2021-12-13 20:38:32 -0300

    feat: add GlobalStyle

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import store from './redux/store'
+import GlobalStyle from './styles/GlobalStyle'
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
+      <GlobalStyle/>
       <App />
     </React.StrictMode>
   </Provider>,
