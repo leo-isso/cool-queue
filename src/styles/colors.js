b67a1e0bc187a0f4141ea5d07b1bbc1dcca8a98f
Leonardo Isso <leoisso.work@gmail.com>
2021-12-13 19:54:12 -0300

    feat: add initial colo schema

---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#00bcd4',
+  primaryDark: '#008394',
+  primaryLight: '#00e5ff',
+  secondary: '#ffc107',
+  white: '#ffffff',
+  black: '#000000'
+}
+
+export default colors
