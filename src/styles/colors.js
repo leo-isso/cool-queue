0b1dc54d3bb53988748e8131b74aac0608662b20
Leonardo Isso <leoisso.work@gmail.com>
2021-12-13 22:40:39 -0300

    feat: add new colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -4,7 +4,9 @@ const colors = {
   primaryLight: '#00e5ff',
   secondary: '#ffc107',
   white: '#ffffff',
-  black: '#000000'
+  black: '#000000',
+  danger: '#f44336',
+  success: '#4caf50'
 }
 
 export default colors
