1007163f0c9c791838afcf9a4d50e5561e217a70
Leonardo Isso <leoisso.work@gmail.com>
2021-12-13 23:49:31 -0300

    feat: add documentation

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,9 +4,15 @@ import { FaWindowClose } from 'react-icons/fa'
 
 import * as Components from './Modal.styles'
 
+/**
+ *  Modal component
+ * @param {Component} children - The children of the modal
+ * @param {string} title - The title of the modal
+ * @param {boolean} show - Whether or not the modal is visible
+ * @param {function} onClose - The function to call when the modal is closed
+ */
 function Modal ({ children, show, onClose, title }) {
   if (!show) return null
-
   return (
     <Components.Background onClick={() => onClose()}>
       <Components.ModalBase onClick={(event) => event.stopPropagation()}>
