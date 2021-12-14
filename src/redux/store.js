38c07184de425c215f789f806dcca2b1336d05ab
Leonardo Isso <leoisso.work@gmail.com>
2021-12-14 09:53:57 -0300

    feat: configure saga middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,16 @@
-import { compose, createStore } from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './reducers'
+import rootSaga from './sagas'
+
+const sagaMiddleware = createSagaMiddleware()
 
 const store = compose(
+  applyMiddleware(sagaMiddleware),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore)(rootReducer)
+
+sagaMiddleware.run(rootSaga)
 
 export default store
