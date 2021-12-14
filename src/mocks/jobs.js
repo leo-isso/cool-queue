402471e3939d7ae68f6605a8186e67bc4885024e
Leonardo Isso <leoisso.work@gmail.com>
2021-12-13 22:58:24 -0300

    feat: add temporary mocks

---
+++ src/mocks/jobs.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    name: 'Job Leonardo',
+    duration: '0.25',
+    id: 1,
+    index: 1,
+    status: 'working',
+    progress: 0
+  },
+  {
+    name: 'Job Json',
+    duration: '10',
+    id: 2,
+    index: 2,
+    status: 'pending',
+    progress: 0
+  },
+  {
+    name: 'Job Juan',
+    duration: '0.25',
+    id: 3,
+    index: 3,
+    status: 'pending',
+    progress: 0
+  },
+  {
+    name: 'Job Jhon',
+    duration: '0.3',
+    id: 4,
+    index: 4,
+    status: 'pending',
+    progress: 0
+  },
+  {
+    name: 'Job Mario',
+    duration: '0.25',
+    id: 5,
+    index: 5,
+    status: 'pending',
+    progress: 0
+  },
+  {
+    name: 'Job Raul',
+    duration: '2.25',
+    id: 6,
+    index: 6,
+    status: 'pending',
+    progress: 0
+  }
+]
