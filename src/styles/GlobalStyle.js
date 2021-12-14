7426715b262f0e197d5c6cf42c78a644c4b4dade
Leonardo Isso <leoisso.work@gmail.com>
2021-12-14 01:43:59 -0300

    feat: fix style for number inputs

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -14,4 +14,15 @@ export default createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   };
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type=number] {
+    -moz-appearance: textfield;
+  }
 `
