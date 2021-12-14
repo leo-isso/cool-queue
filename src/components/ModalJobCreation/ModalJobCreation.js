ad15e8cead08afb54cf84394df4dc5516955aa4b
Leonardo Isso <leoisso.work@gmail.com>
2021-12-14 01:56:49 -0300

    feat: remove form from Modal

--- src/components/ModalJobCreation/ModalJobCreation.js
+++ src/components/ModalJobCreation/ModalJobCreation.js
@@ -1,23 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Form } from 'react-final-form'
 
 import * as Components from './ModalJobCreation.styles'
-import JobCreationForm from './Form'
 import Modal from '../Modal'
+import AddJobToQueue from '../../Forms/AddJobToQueue'
 
 function ModalJobCreation ({ show, onClose }) {
   return (
     <Modal show={show} onClose={onClose} title="Add item to queue">
       <Components.Wrapper>
-        <Form
-          onSubmit={(values) => console.log(values)}
-          validate={(values) => console.log(values)}
-          initialValues={{ timeFormat: 'minutes' }}
-          render={({ handleSubmit, submitting }) => (
-            <JobCreationForm handleSubmit={handleSubmit} submitting={submitting}/>
-          )}
-      />
+        <AddJobToQueue/>
       </Components.Wrapper>
     </Modal>
   )
