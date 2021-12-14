b161e6a3856acf510f31abe5db02e9b134235df1
Leonardo Isso <leoisso.work@gmail.com>
2021-12-13 22:56:19 -0300

    feat: add JobList component

---
+++ src/components/JobList/JobList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import JobCard from '../JobCard'
+import * as Components from './JobList.style'
+
+function JobList ({ jobs }) {
+  return (
+    <Components.Wrapper>
+      {jobs.map(job => (
+        <JobCard
+          key={job.id}
+          job={job}
+          onCancelItem={(job) => console.log(job)}
+          />
+      ))}
+    </Components.Wrapper>
+  )
+}
+
+JobList.propTypes = {
+  jobs: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      duration: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      index: PropTypes.number.isRequired,
+      status: PropTypes.string.isRequired,
+      progress: PropTypes.number.isRequired
+    })
+  ).isRequired
+}
+
+export default JobList
