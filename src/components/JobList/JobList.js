01d769744365af17c85a8b996697c532ab4c9c4b
Leonardo Isso <leoisso.work@gmail.com>
2021-12-14 10:47:01 -0300

    feat: add removePendingJob to component actions

--- src/components/JobList/JobList.js
+++ src/components/JobList/JobList.js
@@ -1,32 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
 
 import JobCard from '../JobCard'
 import * as Components from './JobList.styles'
+import { queueRemovePendingJob } from '../../redux/actions/queue'
 
 /**
  * JobList component
  * @param {object} jobs
  */
 function JobList ({ jobs }) {
+  const dispatch = useDispatch()
   const hasJobs = jobs && jobs.length > 0
+
+  const handleCancelItem = (job) => {
+    dispatch(queueRemovePendingJob(job))
+  }
 
   return (
     <Components.Wrapper>
       {hasJobs
-        ? jobs.map((job) => (
-          <JobCard
-            key={job.id}
-            job={job}
-            onCancelItem={(job) => console.log(job)}
-          />
-        ))
+        ? (
+            jobs.map((job) => (
+              <JobCard
+                key={job.id}
+                job={job}
+                onCancelItem={(job) => handleCancelItem(job)}
+              />
+            ))
+          )
         : (
-          <Components.Empty>
-            <Components.EmptyText>
-              {'No items found :('}
-            </Components.EmptyText>
-          </Components.Empty>
+            <Components.Empty>
+              <Components.EmptyText>{'No items found :('}</Components.EmptyText>
+            </Components.Empty>
           )}
     </Components.Wrapper>
   )
