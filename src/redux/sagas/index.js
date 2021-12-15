027b237e81bbfa1b813496758bff491c7b5d1b57
Leonardo Isso <leoisso.work@gmail.com>
2021-12-15 02:25:33 -0300

    feat: add completedSaga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,9 +1,12 @@
 import { all } from 'redux-saga/effects'
+
+import completedSaga from './completed'
 import pendingSaga from './pending'
 import workingJobSaga from './workingJob'
 
 export default function * rootSaga () {
   yield all([
+    completedSaga(),
     pendingSaga(),
     workingJobSaga()
   ])
