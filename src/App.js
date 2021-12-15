7d552e6ee1983cb6e096aeaa326fc6329e70ae67
Leonardo Isso <leoisso.work@gmail.com>
2021-12-15 02:52:07 -0300

    feat:  auto add pending item to working item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import CurrentJobBanner from './components/CurrentJobBanner'
 import Header from './components/Header'
@@ -7,26 +7,22 @@ import JobList from './components/JobList'
 import Tabs from './components/Tabs'
 
 import { useModal } from './contexts/Modals/Modal.context'
-// import { queueAddWorkingJob } from './redux/actions/queue'
+import { addWorkingJob } from './redux/actions/workingItem'
 
 function App () {
   console.log('render')
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
   const { openModal } = useModal()
 
   const pending = useSelector((state) => state.pending)
   const completed = useSelector((state) => state.completed)
   const workingItem = useSelector((state) => state.working_item)
-  // const { pending, completed, working_item: workingItem } = queue
-  // const noWorkingItem = (!workingItem)
 
-  // useEffect(() => {
-  //   console.log('App.js: useEffect')
-  //   if (!workingItem && pending.length > 0) {
-  //     dispatch(queueAddWorkingJob(pending[0]))
-  //   }
-  // }, [noWorkingItem, pending])
-
+  useEffect(() => {
+    if (!workingItem && pending.length > 0) {
+      dispatch(addWorkingJob(pending[0]))
+    }
+  }, [pending])
   const tabItems = [
     {
       title: 'Pending/Working',
