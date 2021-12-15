a859dd417706abc5e2c4848a3cb950ea2a4602c5
Leonardo Isso <leoisso.work@gmail.com>
2021-12-15 01:41:24 -0300

    fix: change operator

--- src/redux/reducers/size.js
+++ src/redux/reducers/size.js
@@ -5,10 +5,10 @@ const initialState = 0
 export default function sizeReducer (state = initialState, action) {
   switch (action.type) {
     case types.INCREMENT_SIZE:
-      return state++
+      return state + 1
 
     case types.DECREMENT_SIZE:
-      return state--
+      return state - 1
 
     default:
       return state
