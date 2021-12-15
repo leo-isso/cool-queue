893a55cc28c523bc7c31b6ab9ab0bb34c94038d6
Leonardo Isso <leoisso.work@gmail.com>
2021-12-14 23:12:53 -0300

    feat: add inProgressJobSpy

---
+++ src/services/inProgressJobSpy.js
@@ -0,0 +1,39 @@
+import store from '../redux/store'
+import { queueUpdatWorkingJob } from '../redux/actions/queue'
+
+/**
+ * The inProgressJobSpy should be instanced only once
+ * and it should be injected in the inProgressJob service
+ * in order to watch and retrieve info from the current job
+ * It has both methods that supports watching and stoping the interval
+ */
+const inProgressJobSpy = () => ({
+  duration: 0,
+  intervalId: '',
+  progress: 0,
+
+  setDuration (duration) {
+    this.duration = duration
+  },
+
+  setIntervalId (id) {
+    this.intervalId = id
+  },
+
+  stopInterval () {
+    clearInterval(this.intervalId)
+  },
+
+  setProgress (time) {
+    this.progress = parseInt((time * 100) / this.duration)
+  },
+
+  dispatchProgress () {
+    console.log('dispatching progress', this.progress)
+    store.dispatch(queueUpdatWorkingJob({ progress: this.progress }))
+  }
+})
+
+const inProgressJobSpyInstance = inProgressJobSpy()
+
+export default inProgressJobSpyInstance
