1c19ea364b27c6a26149786bd6c2edc4cfe3e499
Leonardo Isso <leoisso.work@gmail.com>
2021-12-15 01:29:24 -0300

    feat: rename file and restructure processes

--- src/redux/sagas/pending.js
+++ src/redux/sagas/pending.js
@@ -1,23 +1,32 @@
-import { put, takeEvery } from 'redux-saga/effects'
+import { put, select, takeEvery } from 'redux-saga/effects'
 
 import * as types from '../types'
 import createJob from '../../services/createJob'
 import updateJob from '../../services/updateJob'
+import { decrementSize, incrementSize } from '../actions/size'
+import { setEmpty, unsetEmpty } from '../actions/empty'
 
-function * queueAddPendingJob (action) {
+function * addPendingJob (action) {
   try {
     const item = yield createJob(action.payload)
     yield put({ type: types.QUEUE_ADD_PENDING_JOB_SUCCESS, payload: item })
+    yield put(incrementSize())
+    yield put(unsetEmpty())
   } catch (error) {
     const { message } = error
     yield put({ type: types.QUEUE_ADD_PENDING_JOB_FAIL, payload: { message } })
   }
 }
 
-function * queueRemovePendingJob (action) {
+function * removePendingJob (action) {
   try {
     const item = yield updateJob(action.payload, { status: 'cancelled' })
     yield put({ type: types.QUEUE_REMOVE_PENDING_JOB_SUCCESS, payload: item })
+    yield put(decrementSize())
+    const size = yield select(state => state.size)
+    if (size === 0) {
+      yield put(setEmpty())
+    }
   } catch (error) {
     const { message } = error
     yield put({ type: types.QUEUE_REMOVE_PENDING_JOB_FAIL, payload: { message } })
@@ -25,8 +34,8 @@ function * queueRemovePendingJob (action) {
 }
 
 function * pendingQueueSaga () {
-  yield takeEvery(types.QUEUE_ADD_PENDING_JOB, queueAddPendingJob)
-  yield takeEvery(types.QUEUE_REMOVE_PENDING_JOB, queueRemovePendingJob)
+  yield takeEvery(types.QUEUE_ADD_PENDING_JOB, addPendingJob)
+  yield takeEvery(types.QUEUE_REMOVE_PENDING_JOB, removePendingJob)
 }
 
 export default pendingQueueSaga
