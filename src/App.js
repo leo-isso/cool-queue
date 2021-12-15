3ccd7f0f9f97e75805cb95d3f9cc9b499d16e23e
Leonardo Isso <leoisso.work@gmail.com>
2021-12-15 18:58:33 -0300

    feat: remove working item from tab

--- src/App.js
+++ src/App.js
@@ -8,12 +8,31 @@ import Tabs from './components/Tabs'
 
 import { addWorkingItem } from './redux/actions/workingItem'
 
+const renderTabs = () => {
+  const pending = useSelector((state) => state.pending)
+  const completed = useSelector((state) => state.completed)
+
+  const tabItems = [
+    {
+      title: 'Pending',
+      content: () => (
+        <JobList jobs={pending} />
+      )
+    },
+    {
+      title: 'Finished/Cancelled',
+      content: () => <JobList jobs={completed} />
+    }
+  ]
+
+  return <Tabs tabItems={tabItems} />
+}
+
 function App () {
   console.log('render')
   const dispatch = useDispatch()
 
   const pending = useSelector((state) => state.pending)
-  const completed = useSelector((state) => state.completed)
   const workingItem = useSelector((state) => state.working_item)
 
   useEffect(() => {
@@ -21,22 +40,12 @@ function App () {
       dispatch(addWorkingItem(pending[0]))
     }
   }, [pending, workingItem])
-  const tabItems = [
-    {
-      title: 'Pending/Working',
-      content: () => <JobList jobs={workingItem ? [workingItem, ...pending] : pending} />
-    },
-    {
-      title: 'Finished',
-      content: () => <JobList jobs={completed} />
-    }
-  ]
 
   return (
     <div className="App">
       <Header />
       <CurrentJobBanner job={workingItem} />
-      <Tabs tabItems={tabItems} />
+      {renderTabs()}
     </div>
   )
 }
