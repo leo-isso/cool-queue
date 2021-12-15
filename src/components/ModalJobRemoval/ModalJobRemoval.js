ad14364558e9cdbf3ba4aef07adcfa589ad403b3
Leonardo Isso <leoisso.work@gmail.com>
2021-12-14 21:47:46 -0300

    feat: add ModalJobRemoval

---
+++ src/components/ModalJobRemoval/ModalJobRemoval.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+
+import * as Components from './ModalJobRemoval.styles'
+import Modal from '../Modal'
+import Button from '../Button'
+
+import { useModal } from '../../contexts/Modals/Modal.context'
+import { queueRemovePendingJob, queueRemoveWorkingJob } from '../../redux/actions/queue'
+
+function ModalJobRemoval ({ job }) {
+  const dispatch = useDispatch()
+  const { closeModal } = useModal()
+
+  const handleCancelItem = () => {
+    if (job.status === 'pending') {
+      dispatch(queueRemovePendingJob(job))
+    } else {
+      dispatch(queueRemoveWorkingJob(job))
+    }
+    closeModal()
+  }
+
+  return (
+    <Modal onClose={closeModal} title="Remove Job">
+      <Components.Wrapper>
+        <Button onClick={handleCancelItem}>Remove</Button>
+        <Button onClick={closeModal}>Cancel</Button>
+      </Components.Wrapper>
+    </Modal>
+  )
+}
+
+ModalJobRemoval.propTypes = {
+  job: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    duration: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    index: PropTypes.number,
+    status: PropTypes.string.isRequired,
+    progress: PropTypes.number.isRequired
+  }).isRequired
+}
+
+export default ModalJobRemoval
