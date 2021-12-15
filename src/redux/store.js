f1f2b9a568d35e0da546741147218fb538fd0862
Leonardo Isso <leoisso.work@gmail.com>
2021-12-15 19:07:02 -0300

    feat: update redux dev tools implementation

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ const sagaMiddleware = createSagaMiddleware()
 
 const store = compose(
   applyMiddleware(sagaMiddleware),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 )(createStore)(rootReducer)
 
 sagaMiddleware.run(rootSaga)
