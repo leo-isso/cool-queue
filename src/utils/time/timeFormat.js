78fe44bfeea2128c36a21e7f857eb37509776619
Leonardo Isso <leoisso.work@gmail.com>
2021-12-14 22:44:27 -0300

    fix: change float to int

--- src/utils/time/timeFormat.js
+++ src/utils/time/timeFormat.js
@@ -2,10 +2,14 @@ export const secondsToMinutes = (seconds) => {
   return parseFloat(seconds / 60).toFixed(2)
 }
 
-export const minutesToSeconds = (minutes) => {
-  return parseFloat(minutes * 60).toFixed(2)
+export const secondsToMiliseconds = (seconds) => {
+  return parseInt(seconds * 1000)
 }
 
-export const secondsToMiliseconds = (seconds) => {
-  return parseFloat(seconds * 1000).toFixed(2)
+export const minutesToSeconds = (minutes) => {
+  return parseInt(minutes * 60)
+}
+
+export const minutesToMiliseconds = (minutes) => {
+  return parseInt(minutes * 60000)
 }
