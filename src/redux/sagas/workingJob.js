b762d901a0ecd8c1a9bd9f153bdb93dc61ebbc10
Leonardo Isso <leoisso.work@gmail.com>
2021-12-14 23:16:24 -0300

    feat: improve working job simulation and remove

--- src/redux/sagas/workingJob.js
+++ src/redux/sagas/workingJob.js
@@ -1,12 +1,19 @@
-import { put, takeEvery } from 'redux-saga/effects'
+import { call, put, takeEvery } from 'redux-saga/effects'
 
 import * as types from '../types'
 import updateJob from '../../services/updateJob'
+import inProgressJob from '../../services/inProgressJob'
+import inProgressJobSpy from '../../services/inProgressJobSpy'
+
+const spy = inProgressJobSpy
 
 function * queueAddWorkingJob (action) {
   try {
     const item = yield updateJob(action.payload, { status: 'working' })
     yield put({ type: types.QUEUE_ADD_WORKING_JOB_SUCCESS, payload: item })
+    yield call(inProgressJob, item, spy)
+    const finishedItem = yield updateJob(action.payload, { status: 'finished' })
+    yield put({ type: types.QUEUE_ADD_COMPLETED_JOB, payload: finishedItem })
   } catch (error) {
     const { message } = error
     yield put({ type: types.QUEUE_ADD_WORKING_JOB_FAIL, payload: { message } })
@@ -15,6 +22,7 @@ function * queueAddWorkingJob (action) {
 
 function * queueRemoveWorkingJob (action) {
   try {
+    yield spy.stopInterval()
     const item = yield updateJob(action.payload, { status: 'cancelled' })
     yield put({ type: types.QUEUE_REMOVE_WORKING_JOB_SUCCESS, payload: item })
   } catch (error) {
