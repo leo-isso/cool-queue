1c2c2ecfc8da8f87f453071aa58e7cf2a94fae30
Leonardo Isso <leoisso.work@gmail.com>
2021-12-15 02:36:36 -0300

    feat: rename file

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -2,12 +2,12 @@ import { all } from 'redux-saga/effects'
 
 import completedSaga from './completed'
 import pendingSaga from './pending'
-import workingJobSaga from './workingJob'
+import workingItemSaga from './workingItem'
 
 export default function * rootSaga () {
   yield all([
     completedSaga(),
     pendingSaga(),
-    workingJobSaga()
+    workingItemSaga()
   ])
 }
